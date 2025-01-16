7952d964640f78cef3d164b8f89b29034e624c50
Rostislav Otevrel <rostislav@otevrel.net>
2025-01-16 15:16:30 +0100

    Add LcdMenu and SimpleRotary support to main application

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
-//* LIBRARIES
+//* LIBRARIES AND HEADERS
 
 #include <Arduino.h>
+
+#include <LcdMenu.h>
+#include <MenuScreen.h>
+#include <MenuItem.h>
+#include <ItemCommand.h>
+#include <ItemValue.h>
+#include <ItemWidget.h>
+#include <widget/WidgetRange.h>
+
+#include <display/LiquidCrystal_I2CAdapter.h>
+#include <renderer/CharacterDisplayRenderer.h>
+#include <input/SimpleRotaryAdapter.h>
+
 #include <SimpleRotary.h>
 #include <LiquidCrystal_I2C.h>
 
@@ -11,56 +24,46 @@
 #define ENC_1 5
 #define ENC_2 4
 
+//* GLOBAL VARIABLES
+
+int turntablePosition = 0;
+
 //* OBJECTS
 
-LiquidCrystal_I2C display(0x27, 20, 4);
-SimpleRotary knob(ENC_1, ENC_2, BTN_2);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+SimpleRotary rotary(ENC_1, ENC_2, BTN_2);
+
+LiquidCrystal_I2CAdapter lcdAdapter(&lcd);
+CharacterDisplayRenderer renderer(&lcdAdapter, 20, 4);
+LcdMenu menu(renderer);
+SimpleRotaryAdapter knob(&menu, &rotary);
+
+//* SCREENS
+
+MENU_SCREEN(mainScreen, mainItems,
+    ITEM_VALUE("Pozice", x, "%d"),
+    ITEM_COMMAND("increment", [](){x++;}),
+    ITEM_COMMAND("decrement", [](){x--;}),
+    ITEM_COMMAND("SEND A", [](){Serial.print("A");}),
+    ITEM_COMMAND("SEND B", [](){Serial.print("B");})
+    );
 
 //* FUNCTIONS
 
 
-
-//* GLOBAL VARIABLES
-
-int ctr = 0;
-int x = 0;
-
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-
-    knob.setTrigger(LOW);
-    display.init();
-    display.backlight();
-    display.print(ctr);
+    renderer.begin();
+    menu.setScreen(mainScreen);
 }
 
-void loop() {
-    int r = knob.rotate();
-    if (r == 1) {
-        ctr++;
-        display.clear();
-        display.print(ctr);
-    }
-    if (r == 2) {
-        ctr--;
-        display.clear();
-        display.print(ctr);
-    }
-    if (knob.push() == 1) {
-        display.clear();
-        display.print("Pressed");
-        switch (x) {
-        case 0:
-            Serial.print('A');
-            x = 1;
-            break;
-        case 1:
-            Serial.print('B');
-            x = 0;
-            break;
-        }
-        delay(1000);
-        display.clear();
-        display.print(ctr);
-    }
+void loop()
+{
+   knob.observe();
+   menu.poll();
+   if (Serial.available() > 0)
+   {
+       char incomingByte = Serial.read();
+   }
 }
