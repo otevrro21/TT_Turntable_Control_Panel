ce8db8e9054c5044d76d867f259fcc32676a41d8
Rostislav Otevrel <rostislav@otevrel.net>
2025-02-18 15:15:19 +0100

    The panel works now.
    Some minor issues like eg. don't press the estop multiple times or you'll break stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-//* LIBRARIES AND HEADERS
+//* LIBRARIES: ---------------------------------------------------------------------------------
 
 #include <Arduino.h>
 
@@ -7,7 +7,7 @@
 #include <MenuItem.h>
 #include <ItemCommand.h>
 #include <ItemValue.h>
-#include <ItemWidget.h>
+#include <ItemSubMenu.h>
 #include <widget/WidgetRange.h>
 
 #include <display/LiquidCrystal_I2CAdapter.h>
@@ -17,18 +17,28 @@
 #include <SimpleRotary.h>
 #include <LiquidCrystal_I2C.h>
 
-//* PINS
+//* PINS: --------------------------------------------------------------------------------------
 
 #define BTN_1 2
 #define BTN_2 3
 #define ENC_1 5
 #define ENC_2 4
 
-//* GLOBAL VARIABLES
+//! GLOBAL VARIABLES: --------------------------------------------------------------------------
 
-int turntablePosition = 0;
+int currentBridgePosition = 0; // Current position of the bridge (side A)
+int futurePosition = 0; // Future position of the turntable set by the user
+bool estopSwitch = false; // for sending stop message to the turntable if interrupt is triggered
+char turntableStatus[20] = "Waiting"; // Status of the turntable, cycles between 'Waiting' and 'Turning'
 
-//* OBJECTS
+unsigned long previousTime = 0;
+unsigned long currentTime = millis();
+
+//* FUNCTION PROTOTYPES: -----------------------------------------------------------------------
+
+void incomingSerial(), switchScreen(int screen), estop();
+
+//* OBJECTS: -----------------------------------------------------------------------------------
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 SimpleRotary rotary(ENC_1, ENC_2, BTN_2);
@@ -38,32 +48,128 @@ CharacterDisplayRenderer renderer(&lcdAdapter, 20, 4);
 LcdMenu menu(renderer);
 SimpleRotaryAdapter knob(&menu, &rotary);
 
-//* SCREENS
+//* SCREENS: -----------------------------------------------------------------------------------
+MENU_SCREEN(rotationScreen, rotationItems,
+    ITEM_BASIC("Rotate the turntable?"),
+    ITEM_COMMAND("Yes", [](){
+        Serial.print(String("P") + futurePosition);
+        strcpy(turntableStatus, "Turning");
+        switchScreen(0);
+        }),
+    ITEM_COMMAND("No", [](){switchScreen(0);})
+);
+
+MENU_SCREEN(selectionScreen, selectionItems,
+    ITEM_BASIC("Select position"),
+    ITEM_COMMAND("Position 1", [](){futurePosition = 0; menu.setScreen(rotationScreen);}),
+    ITEM_COMMAND("Position 2", [](){futurePosition = 1; menu.setScreen(rotationScreen);}),
+    ITEM_COMMAND("Position 3", [](){futurePosition = 2; menu.setScreen(rotationScreen);}),
+    ITEM_COMMAND("Position 4", [](){futurePosition = 3; menu.setScreen(rotationScreen);}),
+    ITEM_COMMAND("Position 5", [](){futurePosition = 4; menu.setScreen(rotationScreen);}),
+    ITEM_COMMAND("Position 6", [](){futurePosition = 5; menu.setScreen(rotationScreen);}),
+    ITEM_COMMAND("Position 7", [](){futurePosition = 6; menu.setScreen(rotationScreen);}),
+    ITEM_COMMAND("Position 8", [](){futurePosition = 7; menu.setScreen(rotationScreen);}),
+    ITEM_COMMAND("Position 9", [](){futurePosition = 8; menu.setScreen(rotationScreen);})
+);
 
 MENU_SCREEN(mainScreen, mainItems,
-    ITEM_VALUE("Pozice", x, "%d"),
-    ITEM_COMMAND("increment", [](){x++;}),
-    ITEM_COMMAND("decrement", [](){x--;}),
-    ITEM_COMMAND("SEND A", [](){Serial.print("A");}),
-    ITEM_COMMAND("SEND B", [](){Serial.print("B");})
-    );
+    ITEM_VALUE("Position", currentBridgePosition, "%d"),
+    ITEM_VALUE("Status", turntableStatus, "%s"),
+    ITEM_SUBMENU("Select position", selectionScreen),
+    ITEM_COMMAND("Home", [](){switchScreen(3);})
+);
 
-//* FUNCTIONS
+MENU_SCREEN(homePromptScreen, homePromptItems,
+    ITEM_BASIC("Home the turntable?"),
+    ITEM_COMMAND("Yes", [](){
+        Serial.print('H');
+        strcpy(turntableStatus, "Homing");
+        menu.setScreen(mainScreen);
+        }),
+    ITEM_COMMAND("No", [](){menu.setScreen(mainScreen);})
+);
 
+MENU_SCREEN(stopScreen, stopItems,
+    ITEM_BASIC("E-STOP TRIGGERED!"),
+    ITEM_COMMAND("Reset", [](){
+        Serial.print('R');
+        strcpy(turntableStatus, "Waiting");
+        menu.setScreen(mainScreen);
+    }),
+    ITEM_COMMAND("Continue", [](){
+        Serial.print('C');
+        strcpy(turntableStatus, "Waiting");
+        menu.setScreen(mainScreen);
+    })
+);
 
-void setup()
-{
+//* MAIN CODE: ---------------------------------------------------------------------------------
+
+void setup() {
     Serial.begin(115200);
+
+    pinMode(BTN_1, INPUT);
+    attachInterrupt(digitalPinToInterrupt(BTN_1), estop, FALLING);
+
     renderer.begin();
-    menu.setScreen(mainScreen);
+    menu.setScreen(homePromptScreen);
 }
 
-void loop()
-{
-   knob.observe();
-   menu.poll();
-   if (Serial.available() > 0)
-   {
-       char incomingByte = Serial.read();
-   }
+void loop() {
+    currentTime = millis();
+    knob.observe();
+    menu.poll();
+    incomingSerial();
+    if (estopSwitch) {
+        if (currentTime - previousTime > 500) {
+            previousTime = currentTime;
+            Serial.print('S');
+        }
+        strcpy(turntableStatus, "Stopped");
+        estopSwitch = false;
+        menu.setScreen(stopScreen);
+    }
+}
+
+void incomingSerial() {
+    if (Serial.available() > 0) {
+        char command = Serial.read();
+        if (command == 'H') {
+            strcpy(turntableStatus, "Home");
+        } else if (command == 'P') {
+            // Wait for the second character
+            delay(10); // Small delay to ensure next character arrives (im so sorry)
+            if (Serial.available() > 0) {
+                char posChar = Serial.read();
+                int pos = posChar - '0';
+                if (pos >= 0 && pos <= 8) {
+                    currentBridgePosition = pos + 1; // + 1 for better user readability (1-9 instead of 0-8)
+                    strcpy(turntableStatus, "Waiting");
+                }
+            }
+        }
+    }
+}
+
+void switchScreen(int screen) {
+    switch (screen) {
+        case 0:
+            menu.setScreen(mainScreen);
+            break;
+        case 1:
+            menu.setScreen(rotationScreen);
+            break;
+        case 2:
+            menu.setScreen(selectionScreen);
+            break;
+        case 3:
+            menu.setScreen(homePromptScreen);
+            break;
+        default:
+            break;
+    }
+}
+
+void estop() {
+    estopSwitch = true;
 }
